8d3b77817805f5b5d5483aaa4665375198c3a4e5
arorah1 <arorah1@emc.com>
2013-10-30 15:19:53 +0530

    Accessiable test

--- src/com/AccessibleTest.java
+++ src/com/AccessibleTest.java
@@ -2,6 +2,7 @@ package com;
 
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class AccessibleTest {
 
@@ -11,13 +12,18 @@ public class AccessibleTest {
 			Class cls = Class.forName("com.HelloWorld");
 			Field privateField = cls.getDeclaredField("private_long_variable");
 			privateField.setAccessible(true);
-			Object o = cls.newInstance();
+			Object obj = cls.newInstance();
 			System.out.println("-------------------Private field original value--------------");
-			System.out.println("private_long_variable="+privateField.get(o));
-			privateField.set(o , 10);
-			System.out.println("-------------------Private field updated value--------------");
-			System.out.println("private_long_variable="+privateField.get(o));
-			
+			System.out.println("private_long_variable="+privateField.get(obj));
+			privateField.set(obj , 10);
+			System.out.println("-------------------Private field updated value---------------");
+			System.out.println("private_long_variable="+privateField.get(obj));
+			Class[] argTypes = new Class[] { String.class };
+			Method method = cls.getDeclaredMethod("increment", argTypes);
+			method.setAccessible(true);
+			Object temp = method.invoke(obj, "20");
+			System.out.println("-------------------Private Method access---------------------");
+			System.out.println("private method return value=" + temp);
 	}
 
 }
